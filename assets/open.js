33836123e452275e88cdb09e5749b9b7dfccd6ff
rdksl <kyselyradek@gmail.com>
2017-08-19 03:20:55 +0200

    Init open

---
+++ assets/open.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+    const moduleCheck = window.location.href.match(/framermodules\.com\/[^\/]+$/)
+    const moduleName = moduleCheck ? moduleCheck[0].replace(/framermodules\.com\//, '') : null
+
+    if (moduleName) {
+        const urlScheme = 'framermodules://' + moduleName
+
+        window.setTimeout(function() {
+            window.location = urlScheme
+        }, 1000)
+    }
+}
+
+const download = () => {
+    window.location = 'http://framermodules.kysely.digital/'
+    return false
+}
