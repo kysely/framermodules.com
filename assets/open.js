fda535e40bd72cb365f7633b6f7367e49857e817
rdksl <kyselyradek@gmail.com>
2017-08-22 02:47:32 +0200

    og:image sizes added

--- assets/open.js
+++ assets/open.js
@@ -1,11 +1,9 @@
 window.onload = () => {
     const moduleCheck = window.location.href.match(/framermodules\.com\/[^\/]+$/)
     const moduleName = moduleCheck ? moduleCheck[0].replace(/framermodules\.com\//, '') : null
-    console.log('module name', moduleName)
 
     if (moduleName) {
         const urlScheme = 'framermodules://' + moduleName
-        console.log(urlScheme)
 
         window.setTimeout(function() {
             window.location = urlScheme
